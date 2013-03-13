f152e3deae285a40f5beabf862541eda50e36d1c
Jason Reilly <jdr0887@renci.org>
2013-03-13 10:56:00 -0400

    removing referenceSequence

--- ncgenes-clean-workflow/src/main/java/edu/unc/mapseq/pipeline/ncgenes/clean/NCGenesCleanPipelineBeanService.java
+++ ncgenes-clean-workflow/src/main/java/edu/unc/mapseq/pipeline/ncgenes/clean/NCGenesCleanPipelineBeanService.java
@@ -4,18 +4,8 @@ import edu.unc.mapseq.pipeline.AbstractPipelineBeanService;
 
 public class NCGenesCleanPipelineBeanService extends AbstractPipelineBeanService {
 
-    private String referenceSequence;
-
     public NCGenesCleanPipelineBeanService() {
         super();
-    }
-
-    public String getReferenceSequence() {
-        return referenceSequence;
-    }
-
-    public void setReferenceSequence(String referenceSequence) {
-        this.referenceSequence = referenceSequence;
     }
 
 }
