b4b1dcc599cfc4ea6b3cb210b3f6d329a8a2dd38
Jason Reilly <jdr0887@renci.org>
2014-06-06 15:41:42 -0400

    initial commit

---
+++ ncgenes-clean-messaging/src/main/java/edu/unc/mapseq/messaging/ncgenes/clean/NCGenesCleanMessageService.java
@@ -0,0 +1,76 @@
+package edu.unc.mapseq.messaging.ncgenes.clean;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NCGenesCleanMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(NCGenesCleanMessageService.class);
+
+    private Connection connection;
+
+    private Session session;
+
+    private ConnectionFactory connectionFactory;
+
+    private NCGenesCleanMessageListener messageListener;
+
+    private String destinationName;
+
+    public NCGenesCleanMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(this.messageListener);
+        this.connection.start();
+
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public NCGenesCleanMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(NCGenesCleanMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
